810e80ba91be6f78084d1c8f742b1caa72543ab1
Vidhi Singh <vidhiii0909@gmail.com>
2026-04-23 10:41:34 +0530

    Enhance CORS settings in app.js
    Updated CORS configuration to include methods and allowed headers.

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ const Patient = require("./models/patient");
 
 var app = express();
 app.use(cors({
-      origin: "https://mediverse-sigma.vercel.app"
+      origin: "https://mediverse-sigma.vercel.app",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 })); 
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
