2a3faf994f8d2019de0ab846ec5191258ed7dfad
Vidhi Singh <vidhiii0909@gmail.com>
2026-04-24 20:37:21 +0530

    Implement loading state in Register component
    Added loading state to the registration process and disabled the Sign Up button during loading.

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import '../App.css'
 import logo from "../assets/logo.png";
 
 export default function Register() {
+   const [loading, setLoading] = useState(false);
    const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [name, setName] = useState("");
@@ -18,6 +19,7 @@ export default function Register() {
 
 
   const handleRegister = async () => {
+     setLoading(true);
     try {
       const endpoint =
         mode === "patient"
@@ -41,9 +43,10 @@ export default function Register() {
     catch (err) {
       console.error(err);
       alert("Server error");
-    }
-
-  };
+    } finally {
+    setLoading(false);
+  }
+};
 
   return (
     <div className="page">
@@ -103,7 +106,8 @@ export default function Register() {
           <input type="password" name="password" placeholder="Password" value={password}
             onChange={(e) => setPassword(e.target.value)} />
 
-          <button className="primary" onClick={handleRegister}>Sign Up</button>
+          <button className="primary" onClick={handleRegister} disabled={loading}>
+  {loading ? "Please wait... (server waking up)" : "Sign Up"}</button>
 
           <p className="bottom" onClick={() => navigate("/")}>
             Already have an account? <span>Login</span>
